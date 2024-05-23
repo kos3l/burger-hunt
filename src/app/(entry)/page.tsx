9ed0c658975da0663acc843791a769dffcc28830
kos3l <kos3l@wp.pl>
2024-05-23 21:57:41 +0200

    test

--- src/app/(entry)/page.tsx
+++ src/app/(entry)/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By s
+            test
             <Image
               src={Zdjecie}
               alt="Vercel Logo"
