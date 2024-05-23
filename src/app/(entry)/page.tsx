fc35874c3a834a5473075d100b632924da5e8475
kos3l <kos3l@wp.pl>
2024-05-23 22:11:50 +0200

    test 4

--- src/app/(entry)/page.tsx
+++ src/app/(entry)/page.tsx
@@ -1,5 +1,7 @@
-import Zdjecie from '@/public/next.svg';
 import Image from 'next/image';
+
+import Zdjecie from '@/public/next.svg';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
